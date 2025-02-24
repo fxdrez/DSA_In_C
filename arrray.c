2191ebdad645000aa4a3fa5100f9ab521bedb9eb
Nirmal Gajurel <156599667+fxdrez@users.noreply.github.com>
2025-02-24 19:45:51 +0545

    run time memory allocated

--- arrray.c
+++ arrray.c
@@ -1,17 +1,36 @@
+// #include <stdio.h>
+// int main()
+// {
+//   int arr[5];
+//   // insert the element
+//   for (int i = 0; i <= 4; i++)
+//   {
+//     printf("\nEnter the '%d' elements of array:- ", i + 1);
+//     scanf("%d", &arr[i]);
+//   }
+//   // display element
+//   for (int i = 0; i <= 4; i++)
+//   {
+//     printf("\nArray element '%d' :- %d", i + 1, arr[i]);
+//   }
+//   return 0;
+// }
+
 #include <stdio.h>
 int main()
 {
-  int arr[5];
-  // insert the element
-  for (int i = 0; i <= 4; i++)
+  int n;
+  printf("Enter the number of array element->");
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i <= n - 1; i++)
   {
-    printf("\nEnter the '%d' elements of array:- ", i + 1);
+    printf("Enter the '%d' element ->", i + 1);
     scanf("%d", &arr[i]);
   }
-  // display element
-  for (int i = 0; i <= 4; i++)
+  printf("\nArray Element");
+  for (int i = 0; i <= n - 1; i++)
   {
-    printf("\nArray element '%d' :- %d", i + 1, arr[i]);
+    printf("%d is the '%d' element\n", arr[i], i + 1);
   }
-  return 0;
 }
