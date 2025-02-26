be8098e711882ba672f09a5597fceb76f0660b2d
Nirmal Gajurel <156599667+fxdrez@users.noreply.github.com>
2025-02-26 10:55:06 +0545

    Insertion, deletation using switch case

--- arrray.c
+++ arrray.c
@@ -16,21 +16,110 @@
 //   return 0;
 // }
 
+// #include <stdio.h>
+// int main()
+// {
+//   int n;
+//   printf("Enter the number of array element->");
+//   scanf("%d", &n);
+//   int arr[n];
+//   for (int i = 0; i <= n - 1; i++)
+//   {
+//     printf("Enter the '%d' element ->", i + 1);
+//     scanf("%d", &arr[i]);
+//   }
+//   printf("Array Elements\n");
+//   for (int i = 0; i <= n - 1; i++)
+//   {
+//     printf("%d is the '%d' element\n", arr[i], i + 1);
+//   }
+// }
+
 #include <stdio.h>
 int main()
 {
-  int n;
-  printf("Enter the number of array element->");
-  scanf("%d", &n);
-  int arr[n];
-  for (int i = 0; i <= n - 1; i++)
+  int size, arr[100], num, pos, base, del;
+  printf("\nEnter the array size-> ");
+  scanf("%d", &size);
+  if (size > 100)
   {
-    printf("Enter the '%d' element ->", i + 1);
-    scanf("%d", &arr[i]);
+    printf("\nYou Exceed the limit; Overflow");
   }
-  printf("Array Elements\n");
-  for (int i = 0; i <= n - 1; i++)
+  else
   {
-    printf("%d is the '%d' element\n", arr[i], i + 1);
+    for (int i = 0; i <= (size - 1); i++)
+    {
+      printf("\nEnter the '%d' element :- ", i + 1);
+      scanf("%d", &arr[i]);
+    }
   }
+  do
+  {
+    printf("\nEnter the number for operation; 1-Output 2-Insert Value 3-Deletion 0-Exit :->");
+    scanf("%d", &base);
+    switch (base)
+    {
+    // Display
+    case 1:
+      for (int i = 0; i <= (size - 1); i++)
+      {
+        printf("\nThe '%d' element is : %d", i + 1, arr[i]);
+      }
+      break;
+
+    // Insertion
+    case 2:
+      printf("\nEnter the value to be inserted: ");
+      scanf("%d", &num);
+      printf("\nEnter the position where the value is to be inserted: ");
+      scanf("%d", &pos);
+      if (pos <= 0 || pos > size + 1)
+      {
+        printf("\nInvalid Position!!");
+      }
+      else
+      {
+        for (int i = size - 1; i >= pos - 1; i--)
+        {
+          arr[i + 1] = arr[i];
+        }
+        arr[pos - 1] = num;
+        size++;
+        for (int i = 0; i <= (size - 1); i++)
+        {
+          printf("\nThe '%d' element is : %d", i + 1, arr[i]);
+        }
+      }
+      break;
+
+    // Deletion
+    case 3:
+      printf("\nEnter the position of data to be deleted : ");
+      scanf("%d", &del);
+      if (del <= 0 || del > size + 1)
+      {
+        printf("\nInvalid Position!!");
+      }
+      else
+      {
+        for (int i = del - 1; i <= size - 1; i++)
+        {
+          arr[i] = arr[i + 1];
+        }
+        size--;
+        for (int i = 0; i <= (size - 1); i++)
+        {
+          printf("\nThe '%d' element is : %d", i + 1, arr[i]);
+        }
+      }
+      break;
+    case 0:
+      printf("\nProcess Terminated!!");
+      break;
+    default:
+      printf("\nInvalid Operation!!");
+      break;
+    }
+  } while (base != 0);
+  return 0;
 }
