a1b4731e5c65d5698b25301309b593f96d902093
Nirmal Gajurel <156599667+fxdrez@users.noreply.github.com>
2025-03-09 14:07:10 +0545

    linked list implemented

---
+++ ll.c
@@ -0,0 +1,38 @@
+// linked list implementation
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  struct node
+  {
+    int data;
+    struct node *next;
+  };
+  struct node *head = 0, *newnode, *temp;
+  int choice;
+  while (choice != 0)
+  {
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter the data :->");
+    scanf("%d", &newnode->data);
+    newnode->next = 0;
+    if (head == 0)
+    {
+      head = temp = newnode;
+    }
+    else
+    {
+      temp->next = newnode;
+      temp = newnode;
+    }
+    printf("Do you want to continue? press any number to continue or press 0 to terminate...->");
+    scanf("%d", &choice);
+  }
+  temp = head;
+  while (temp != 0)
+  {
+    printf("\n%d", temp->data);
+    temp = temp->next;
+  }
+  return 0;
+}
